cc8ba25a911779f43fb539b1fce4211186477a95
eliyamelamed1 <eliyamelamed1@email.com>
2022-07-12 16:35:40 +0300

    improved setup

--- server/src/controllers/account.js
+++ server/src/controllers/account.js
@@ -1,7 +1,7 @@
 const asyncWrapper = require('../middleware/asyncWrapper');
 
 const trying = asyncWrapper((req, res) => {
-    res.status(200).json();
+    res.status(200).json({ massage: '1234' });
 });
 
 module.exports = {
